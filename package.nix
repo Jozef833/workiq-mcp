f5e1d7f34882a9a6f9a339549e56038c1e55aa85
Jozef833 <172046463+Jozef833@users.noreply.github.com>
2026-04-03 21:22:04 +0000

    Downgrade to 0.2.8 to fix broken auth in 0.4.0
    0.4.0 has a known auth bug (microsoft/work-iq#64, #87) where
    ask_work_iq always returns a generic error. Additionally, 0.4.0's
    libmsalruntime.so broker writes IPC data to stdout, corrupting
    the MCP protocol stream. 0.2.8 uses browser-based auth that works
    correctly and doesn't require the native MSAL broker or its many
    dependencies (curl, gtk3, webkit, dbus, etc).

--- package.nix
+++ package.nix
@@ -3,33 +3,14 @@
   stdenv,
   fetchurl,
   autoPatchelfHook,
-
-  # workiq binary deps
   glibc,
   gcc-unwrapped,
   icu,
   openssl,
   zlib,
-
-  # libmsalruntime.so deps
-  curl,
-  dbus,
-  util-linux,
-  gtk3,
-  webkitgtk_4_1,
-  libsecret,
-  libsoup_3,
-  glib,
-  pango,
-  harfbuzz,
-  atk,
-  cairo,
-  gdk-pixbuf,
-  libx11,
-  p11-kit,
 }:
 let
-  version = "0.4.0";
+  version = "0.2.8";
 
   platformMap = {
     "x86_64-linux" = "linux-x64";
@@ -48,7 +29,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@microsoft/workiq/-/workiq-${version}.tgz";
-    hash = "sha256-sTG5YKZSmE57YlaJ6xRaBAewi9qJmsF48lsOxCJZXyA=";
+    hash = "sha256-K8/Ve05srYahyTFpemVaxajK1cJw5v9GGpFjDdWGJPc=";
   };
 
   sourceRoot = ".";
@@ -58,55 +39,26 @@ stdenv.mkDerivation {
   ];
 
   buildInputs = [
-    # workiq binary deps
     glibc
     gcc-unwrapped.lib
     icu
     openssl
     zlib
-
-    # libmsalruntime.so deps
-    curl
-    dbus
-    util-linux.lib
-    gtk3
-    webkitgtk_4_1
-    libsecret
-    libsoup_3
-    glib
-    pango
-    harfbuzz
-    atk
-    cairo
-    gdk-pixbuf
-    libx11
-    p11-kit
   ];
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/bin $out/libexec $out/lib
+    mkdir -p $out/bin $out/libexec
 
     cp package/bin/${platformDir}/workiq $out/libexec/workiq
     chmod +x $out/libexec/workiq
-
-    # libmsalruntime must be both next to the binary (for .NET P/Invoke)
-    # and in $out/lib (for autoPatchelfHook RPATH resolution)
-    if [ -f package/bin/${platformDir}/libmsalruntime.so ]; then
-      cp package/bin/${platformDir}/libmsalruntime.so $out/lib/
-      cp package/bin/${platformDir}/libmsalruntime.so $out/libexec/
-    fi
-    if [ -f package/bin/${platformDir}/libmsalruntime.dylib ]; then
-      cp package/bin/${platformDir}/libmsalruntime.dylib $out/lib/
-      cp package/bin/${platformDir}/libmsalruntime.dylib $out/libexec/
-    fi
 
     # Manual wrapper to avoid makeWrapper renaming the binary
     # (.workiq-wrapped breaks .NET's System.CommandLine root command parser)
     cat > $out/bin/workiq <<WRAPPER
     #!${stdenv.shell}
-    export LD_LIBRARY_PATH="$out/lib:${
+    export LD_LIBRARY_PATH="${
       lib.makeLibraryPath [
         icu
         openssl
@@ -120,7 +72,7 @@ stdenv.mkDerivation {
     runHook postInstall
   '';
 
-  # .NET self-contained binaries are corrupted by strip
+  # .NET self-contained binaries embed an assembly bundle that strip corrupts
   dontStrip = true;
 
   meta = {
